c3430edbb9c542e23525bff2487bb94190686fdc
Magnus Freiholtz <magnus.freiholtz@gmail.com>
2024-06-07 22:47:17 +0200

    add deps and dummy design

--- app/root.tsx
+++ app/root.tsx
@@ -6,6 +6,14 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
+import { type LinksFunction } from "@remix-run/node";
+
+import tailwindStyleSheetUrl from "./styles/tailwind.css?url";
+
+export const links: LinksFunction = () => {
+  return [{ rel: "stylesheet", href: tailwindStyleSheetUrl }];
+};
+
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
@@ -15,8 +23,45 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
-      <body>
-        {children}
+      <body className="bg-purple-600">
+        <div className=" max-w-5xl mx-auto">
+          <header className="border-b-2 border-b-yellow-300 text-yellow-400 py-4">
+            <nav className="flex flex-row justify-between items-center">
+              <h3 className=" font-black text-2xl">Manges website</h3>
+              <ul className="flex space-x-4">
+                <li>
+                  <a href="/">Home</a>
+                </li>
+                <li>
+                  <a href="/about">About</a>
+                </li>
+              </ul>
+            </nav>
+          </header>
+          <div className="flex flex-row gap-4 py-4">
+            <main className="flex-1">{children}</main>
+            <div className="w-56 flex flex-col gap-6 ">
+              <div className="border-yellow-400 border">
+                <h4 className="font-bold text-purple-600 bg-yellow-400 p-2">
+                  Poll
+                </h4>
+                <div className="p-2 bg-yellow-200">
+                  Do you like the new design?
+                </div>
+              </div>
+              <div>
+                <h4 className="font-bold text-purple-600 bg-yellow-400 p-2">
+                  Bannerbyte
+                </h4>
+                <div className="p-2 bg-yellow-200">
+                  <div className="py-2 px-2 bg-green-500 text-green-950">
+                    Jojjeman
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
         <ScrollRestoration />
         <Scripts />
       </body>
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -9,33 +9,32 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/start/quickstart"
-            rel="noreferrer"
-          >
-            5m Quick Start
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/start/tutorial"
-            rel="noreferrer"
-          >
-            30m Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className="flex flex-col gap-4 text-purple-200">
+      <div>
+        <h1 className="text-xl font-black text-yellow-400">New design</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </div>
+      <div>
+        <h2 className="text-xl font-black text-yellow-400 pb-2">Guest book</h2>
+        <div className="flex flex-col gap-4">
+          <div className="border-b border-b-purple-400 pb-4">
+            <div className="font-bold">Nice!</div>
+            <div>/Jalle</div>
+          </div>
+          <div>
+            <div className="font-bold">Wow!</div>
+            <div>/Kalle</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
